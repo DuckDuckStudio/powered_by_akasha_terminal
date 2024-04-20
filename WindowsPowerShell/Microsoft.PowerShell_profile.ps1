2bab2384b03411d5115770b8bf5e2df4adb80c13
Duck Studio <Yzcbs123@163.com>
2024-04-20 18:10:34 +0800

    Delete WindowsPowerShell directory

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,5 +0,0 @@
-function prompt {
-    Write-Host "[Power by 虚空终端] " -NoNewline -ForegroundColor Cyan
-    return "PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
-}
-# Code by DuckStudio
