7875355e64ad03ff94e58ff38484418313af9b37
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-04-20 18:06:44 +0800

    PowerShell实现代码

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+function prompt {
+    Write-Host "[Power by 虚空终端] " -NoNewline -ForegroundColor Cyan
+    return "PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
+}
+# Code by DuckStudio
